f6a8527c6b51ac44b417c15b65d91c678cb15656
Joao Paulo <joao.aguiar@gmail.com>
2013-06-29 18:59:16 -0300

    alteração classe agenda

--- AgendaGit/src/com/tcc/git/Agenda.java
+++ AgendaGit/src/com/tcc/git/Agenda.java
@@ -1,5 +1,30 @@
 package com.tcc.git;
 
 public class Agenda {
+	
+	private String nome;
+	
+	private String numero;
+	
+	public Agenda (){
+		this.nome = "Joao";
+		this.numero = "(85)8888-8888";
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
 }
